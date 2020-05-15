b72dba48378d6134bd35c26782c7df2511f88273
edoprob <mauricio.prob@gmail.com>
2020-05-15 01:36:53 -0300

    final export

--- HtmlParser.js
+++ HtmlParser.js
@@ -1,7 +1,7 @@
 var ejs = require("ejs");
  class HtmlParser{
-     static Parse(table){
-         
+     static async Parse(table){
+         return await ejs.renderFile('./table.ejs', {header: table.header, rows: table.rows});
      }
  }
  module.exports = HtmlParser;
--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var Reader = require("./Reader");
 var Processor = require("./Processor");
+var Writer = require("./Writer");
 var Table = require("./Table");
+var HtmlParser = require("./HtmlParser");
+var PDFWriter = require("./PDFWriter");
 
 
 var leitor = new Reader();
+var escritor = new Writer();
 
 
 async function main(){
     var data = await leitor.Read("./users.csv");
     var dataProcessed = Processor.Process(data);
-    var usuarios = new Table(dataProcessed);
-    console.log(usuarios.header)
-    console.log(usuarios.rows)
     
+    var usuarios = new Table(dataProcessed);
+    
+    var html = await HtmlParser.Parse(usuarios);
+
+    escritor.Write(Date.now()+".html", html);
+    PDFWriter.WritePDF(Date.now()+".pdf", html)
 
 }
 main();
